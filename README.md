c33ab6e541741b9d5524da08cb2a195471412f0c
estebanfern <estebangfernandeza@gmail.com>
2022-12-26 14:49:58 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Consumo de la API de Rick and Morty con Ajax.
---
+++ main.js
@@ -0,0 +1,32 @@
+var cont = 1;
+var lnk = "https://rickandmortyapi.com/api/character?page=";
+for (let k = 1; k <=42 ; k++){
+    $.ajax({
+        url: lnk + cont,
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(data){
+                data.results.forEach(element => {
+                    console.log(element)
+                    let name = element.name;
+                    let img = element.image;
+                    let state = element.status;
+                    let tarj = '<article><div class="image-container"><img src="'+img+'" alt="Personaje"></img></div><h2></h2>' + name + '</h2><br><span>' + state + '</span></article>';
+                    $("#main").append(tarj);
+                });
+                data.info.next;
+        }
+    });    
+}
+
+
+//Model
+/* <article>
+    <div class="image-container">
+        <img src="https://rickandmortyapi.com/api/character/avatar/10.jpeg" alt="Personaje"></img>
+    </div>
+    <h2>Personaje</h2>
+    <span>Estado de Personaje</span>
+</article> */
+
+
